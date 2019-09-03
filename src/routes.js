89cc5027d726a9e8a77a29a7af928ab7a2889977
Nirut Khemasakchai <rutcreate@gmail.com>
2019-09-04 00:36:25 +0700

    Add rate limiter

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,25 @@
 import express from 'express'
+import RateLimit from 'express-rate-limit'
+import RateLimitMongo from 'rate-limit-mongo'
 import HomeController from './controllers/home'
 import ShortenUrlController from './controllers/shorten-url'
+
+// create api rate limiter
+const shortenUrlRateLimiter = RateLimit({
+    store: new RateLimitMongo({
+        uri: process.env.DATABASE_URL
+    }),
+    windowMs: parseInt(process.env.RATE_LIMIT_MS) * 1000,
+    max: parseInt(process.env.RATE_LIMIT_MAX),
+    message: {
+        errors: [
+            {
+                param: 'url',
+                msg: 'Too many requests, please try again later'
+            }
+        ]
+    }
+})
 
 // create new router
 const router = express.Router()
@@ -8,6 +27,6 @@ const router = express.Router()
 // register routes
 router.get('/', HomeController.index)
 router.get('/:code', ShortenUrlController.redirect)
-router.post('/shorten-url', ShortenUrlController.create)
+router.post('/shorten-url', shortenUrlRateLimiter, ShortenUrlController.create)
 
 export default router
